2bf7b6212da44c608a9d2f13dbd9cc15a50dc38d
Jason M <46291780+JasonMorta@users.noreply.github.com>
2024-09-14 09:01:33 +0200

    updates

---
+++ .history/content_20240914085855.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.getElementById('onetrust-consent-sdk');
+  if (element) {
+    element.style.display = 'none';
+  }
+});
